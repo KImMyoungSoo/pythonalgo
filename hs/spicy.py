b5ce5a6721135ed549d2de2a47707c7ac07a9cd6
NamHyunSil <hsilnam3@gmail.com>
2019-09-02 20:01:30 +0900

    spicy

---
+++ hs/spicy.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    result = 0
+    while(scoville[0] < K):
+        try : 
+            result = heapq.heappop(scoville) + 2*heapq.heappop(scoville)
+        except Exception as e:
+            break
+        heapq.heappush(scoville, result)
+        answer +=1
+    if result < K:
+        answer = -1
+    return answer
