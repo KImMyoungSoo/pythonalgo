9926f8627aed35a7d68ff98b86102cebb94b68a2
KimMyoungSoo <5qndjddl6@gmail.com>
2019-09-02 19:57:38 +0900

    python spicier code

---
+++ ms/spicy.py
@@ -0,0 +1,14 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K :
+        if len(scoville) == 1 :
+            return -1
+        answer = answer + 1
+        one = heapq.heappop(scoville)
+        two = heapq.heappop(scoville)
+        new = one + two*2
+        heapq.heappush(scoville, new)
+    return answer
